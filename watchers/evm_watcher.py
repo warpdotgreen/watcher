cadb6cdf196ce508fbc8ab6970b9c767b44da237
yakuhito <y@kuhi.to>
2024-04-28 10:25:51 +0300

    EVM received message watcher

--- watchers/evm_watcher.py
+++ watchers/evm_watcher.py
@@ -97,6 +97,7 @@ class EVMWatcher:
 
         return nonce + 1, new_min_height
 
+
     async def sentMessageWatcher(self):
         web3 = self.getWeb3()
         db = self.getDb()
@@ -121,25 +122,97 @@ class EVMWatcher:
             next_nonce, start_height = resp
 
 
+    async def syncReceivedMessageEvents(self, web3, db, contract, start_height: int) -> int | None: # new start height
+        query_start_height = start_height
+        query_end_height = query_start_height + self.max_block_range - 1
+
+        time_to_stop = False
+        logs = []
+        while not time_to_stop:
+            current_block_height = web3.eth.block_number
+            if query_end_height > current_block_height:
+                query_end_height = current_block_height
+                time_to_stop = True
+
+            if not time_to_stop:
+                self.log(f"Long query for {self.network_id} from {query_start_height} to {query_end_height} (normal if catching up)")
+
+            logs = contract.events.MessageReceived().get_logs(
+                fromBlock=query_start_height,
+                toBlock=query_end_height,
+            )
+
+            logs = [_ for _ in logs]
+            if len(logs) > 0:
+                break
+        
+            query_start_height = query_end_height
+            query_end_height = query_start_height + self.max_block_range - 1
+      
+        # query_start_height will be the last used query_end_height
+        new_min_height = query_start_height
+
+        if len(logs) == 0:
+            return None
+      
+        for event in logs:
+            message_nonce = event.args.nonce
+            self.log(f"Found new received message {event.args.source_chain.decode()}-{message_nonce.hex()} at block {event.blockNumber}")
+
+            msg: Message | None = db.query(Message).filter(and_(
+                Message.nonce == message_nonce,
+                Message.source_chain == event.args.source_chain,
+                Message.destination_chain == self.network_id.encode()
+            )).first()
+
+            if msg is None:
+                self.log(f"Message {event.args.source_chain.decode()}-{message_nonce.hex()} not found in the database; waiting for listener on the other side to add it...")
+            while msg is None:
+                msg = db.query(Message).filter(and_(
+                    Message.nonce == message_nonce,
+                    Message.source_chain == event.args.source_chain,
+                    Message.destination_chain == self.network_id.encode()
+                )).first()
+                await asyncio.sleep(5)
+
+            msg.status = MessageStatus.RECEIVED
+            db.commit()
+
+        return new_min_height
+    
+
     async def receivedMessageWatcher(self):
         web3 = self.getWeb3()
         db = self.getDb()
 
+        latest_message_in_db = db.query(Message).filter(
+            Message.destination_chain == self.network_id.encode()
+        ).order_by(Message.nonce.desc()).first()
+
+        start_height: int = latest_message_in_db.block_number - 1 if latest_message_in_db is not None else self.min_height
+
         portal = web3.eth.contract(address=self.portal_address, abi=PORTAL_EVENTS_ABI)
 
         while True:
-            await asyncio.sleep(5)
+            self.log(f"Checking for received message from height {start_height}...")
+            resp = await self.syncReceivedMessageEvents(web3, db, portal, start_height)
+
+            if resp is None:
+                await asyncio.sleep(60)
+                continue 
+
+            start_height = resp
 
 
     def start(self, loop):
-      self.log(f"Starting...")
+        self.log(f"Starting...")
 
-      self.tasks.append(
-          loop.create_task(self.sentMessageWatcher())
-      )
-      self.tasks.append(
-          loop.create_task(self.receivedMessageWatcher())
-      )
+        #   self.tasks.append(
+        #       loop.create_task(self.sentMessageWatcher())
+        #   )
+        self.tasks.append(
+            loop.create_task(self.receivedMessageWatcher())
+        )
 
     def stop(self):
         self.log(f"Stopping...")
